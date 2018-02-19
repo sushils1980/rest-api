4e6ce8438e332289b064fda6d58e0c9dc8fdf3bb
sushil <sushils@interrait.com>
2018-02-19 12:36:37 +0530

    Adding PATCH /todos/:id call

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
+const _ = require("lodash");
 const {ObjectID} = require("mongodb");
-var express = require("express");
-var bodyParser = require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var {mongoose} = require("./db/mongoose");
-var {Todo} = require("./models/todo");
-var {user} = require("./models/user");
+const {mongoose} = require("./db/mongoose");
+const {Todo} = require("./models/todo");
+const {user} = require("./models/user");
 
 const port = process.env.PORT||3000;
 
@@ -65,6 +66,35 @@ app.delete("/todos/:id", (req, res) => {
         res.send({todo});
     }).catch((err) => {
         res.status(400).send({});
+    });
+});
+
+app.patch("/todos/:id", (req, res) => {
+    var id = req.params.id;
+    var body = _.pick(req.body, ["text", "completed"]);
+
+    if(!ObjectID.isValid(id)){
+        console.log("Object is not valid");
+        return res.status(404).send({});
+    }    
+
+    if(_.isBoolean(body.completed) && body.completed){
+        body.completedAt = new Date().getTime();
+    }
+    else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set:body}, {new: true}).then((todo) => {
+        if(!todo){
+            console.log("Object not found");
+            return res.status(404).send({})
+        }
+
+        res.send({body});
+    }).catch((err) => {
+        res.send(400).send({});
     });
 });
 
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,5 +1,6 @@
-var expect = require("expect");
-var request = require("supertest");
+const _ = require("lodash");
+const expect = require("expect");
+const request = require("supertest");
 const {ObjectID} = require("mongodb");
 
 const {app} = require("./../server");
